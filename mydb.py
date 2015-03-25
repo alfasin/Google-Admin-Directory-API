35158e0a926d93e47822fafc36625f5d32aae88f
Nir Alfasi <alfasi@netflix.com>
2015-03-24 21:18:40 -0700

    adding interface mydb

---
+++ mydb.py
@@ -0,0 +1,10 @@
+# Implement this interface to provide the full path to
+# the .p12 file as well as the account/service emails
+def get_path_to_p12():
+    pass
+
+def get_email():
+    pass
+
+def get_service_email():
+    pass
