03863572611590c1a932289b19556c74af5559b3
Nir Alfasi <alfasi@netflix.com>
2015-03-24 21:04:14 -0700

    inital commit

--- README.md
+++ README.md
@@ -1,1 +1,25 @@
 # GoogleAPI
+
+If you ever worked with Google APIs you already know how this world is full of pain:
+minimal documentation, almost no code-examples, unclear error messages and etc...
+
+This repo was created to help other people avoid the pain that I went through.
+
+## Content
+
+Starting with [admin directory APIs](https://developers.google.com/admin-sdk/directory/) we'll go through some examples of using the [groups](https://developers.google.com/admin-sdk/directory/v1/reference/groups) and [members](https://developers.google.com/admin-sdk/directory/v1/reference/members) SDK and in addition to that we'll see how to implement REST calls to *groups*.
+
+## Important
+In order to use Google APIs you first have to register in [Google cloud console](https://console.developers.google.com/), create a project and set credentials (on the left menu-bar). Attention: all the examples that we'll use are server-side, which means you have to create keys by clicking 
+Once you clicked on "create key" and save the generate .p12 file in a directory which is accessible from your code. When you generate the .p12 file you'll also receive an "account email" (which is your email by default, but assuming you're admin, you can create another email for your organization and use it) and "service account email" which you'll have to use in different cases (I know it's confusing - don't blame me - blame Google). The "service account email" is generated for you and has an ugly format that looks like this: 390109889102-qmmpidjfyadcvb9pek054mmnq0sdfppr@developer.gserviceaccount.com
+
+In order to make your life easier I'll use the naming convention: 
+```
+FULL_PATH_TO_P12
+ACCOUNT_EMAIL
+SERVICE_EMAIL
+```
+
+When time allows, I'll add code examples for using [Google spreadsheet API](https://developers.google.com/google-apps/spreadsheets/) as well.
+
+Enjoy!
---
+++ google_groups.py
@@ -0,0 +1,61 @@
+#!/usr/bin/python
+import httplib2
+import json
+from oauth2client.client import SignedJwtAssertionCredentials
+from urllib import urlencode
+# The module mydb should be implemented by you - provide your credentials
+from mydb import get_path_to_p12, get_email, get_service_email
+
+FULL_PATH_TO_P12 = get_path_to_p12()
+ACCOUNT_EMAIL = get_email()
+SERVICE_EMAIL = get_service_email()
+
+
+def get_group_members(group):
+    url = 'https://www.googleapis.com/admin/directory/v1/groups/{}/members'.format(group['email'])
+    return call_google_api("GET", url)
+
+
+## The following function returns 400 error which is documented here: http://stackoverflow.com/questions/29105310/getting-missing-required-field-member-when-trying-to-add-a-member-to-a-google
+## I'm still hoping that someone that found a workaround will reply to the Stackoverflow thread - once it happens I'll update this file
+# def add_group_members(group, payload=False):
+#     url = 'https://www.googleapis.com/admin/directory/v1/groups/{}/members'.format(group)
+#     return call_google_api("POST", url, payload)
+
+
+def call_google_api(method, url, payload=False):
+    content = {}
+    try:
+        http = get_conn()
+        if payload:
+            (resp, content) = http.request(uri=url, method=method, body=urlencode(payload))
+        else:
+            (resp, content) = http.request(uri=url, method=method)
+    except Exception as e:
+        print "Failed to post request to [{}] due to: {}".format(url, e)
+    return json.loads(content)
+
+
+def get_conn():
+    with open(FULL_PATH_TO_P12) as f:
+        private_key = f.read()
+
+    OAUTH_SCOPE = ['https://www.googleapis.com/auth/admin.directory.group.member',
+                   'https://www.googleapis.com/auth/admin.directory.group']
+
+    credentials = SignedJwtAssertionCredentials(SERVICE_EMAIL, private_key, OAUTH_SCOPE, sub=ACCOUNT_EMAIL)
+    http = httplib2.Http()
+    return credentials.authorize(http)    
+
+
+# Unit test
+if __name__ == '__main__':
+    payload = {
+        "email": "alfasin@your-organization.com'",
+        "role": "MEMBER",
+    }
+    print "\n ---------------------------------- \n"
+    print "calling add_group_member('alfasin@your-organization.com', 'google-group-example@your-organization.com')"
+    res = get_group_members({'email': 'google-group-example@your-organization.com'})
+    print json.dumps(res, indent=4, sort_keys=True)
+    print "\n ---------------------------------- \n"
