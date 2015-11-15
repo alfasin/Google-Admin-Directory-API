5c17fb07d45f2238aaf6f625d17930b8f9447304
Nir Alfasi <alfasin@gmail.com>
2015-11-14 17:45:09 -0800

    Update mydb.py

--- mydb.py
+++ mydb.py
@@ -1,10 +1,10 @@
 # Implement this interface to provide the full path to
 # the .p12 file as well as the account/service emails
 def get_path_to_p12():
-    pass
+    raise NotImplemented("")
 
 def get_email():
-    pass
+    raise NotImplemented("")
 
 def get_service_email():
-    pass
+    raise NotImplemented("")
